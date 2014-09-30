6a142738f9b04e7ba712332069c9fb9ec715e4a5
Cyfer <bac369@nyu.edu>
2014-09-30 14:53:48 -0400

    All Parts Done Fixed Part 1

--- exercises-ch05.js
+++ exercises-ch05.js
@@ -1,3 +1,4 @@
+//*Brandon Cypel bac369@nyu.edu*//
 var arrays = [[2, 4, 6], [8], [10, 12]];
 arrays = arrays.reduce(function( parts, first){
 	return parts.concat(first);
@@ -7,23 +8,22 @@ console.log(arrays);
 var aniRay= ['aardvark', 'abbreviate', 'abacuses', 'abandoners', 'abalones'];
 var numbs = [9, 48, 204, 528942];
 var every = function( ray){
-	var truth = true;
-	ray.forEach(function(part){
-		if ( part % 3 !== 0){
-			truth = false;
+	for(var i = 0; i < ray.length; i++){
+		if ( ray[i] % 3 !== 0){
+			return false;
 		};
-	});
-	return truth;
+	};
+	return true;
 };
 
 var some= function(animals){
 	var truth = false
-	animals.forEach(function(part){
-		if (part.length === 9){
-			truth = true;
+	for(var i = 0; i < animals.length; i++){
+		if (animals[i].length === 9){
+			return true;
 		};
-	});
-		return truth;
+	};
+		return false;
 };
 console.log("Running every... " + every(numbs));
 console.log ("Running some... " + some(aniRay));
